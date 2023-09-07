246c9a410c309e43516b4be071ee3354afdf103d
Sara <saracorraless06@gmail.com>
2023-09-07 22:46:10 +0200

    keyboard structure

--- app/src/main/java/com/scs/calculatorxml/MainActivity.kt
+++ app/src/main/java/com/scs/calculatorxml/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.scs.calculatorxml
 
 
+import android.icu.text.ListFormatter.Width
 import android.os.Bundle
+import android.view.Gravity
+import android.widget.Button
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 
@@ -13,9 +16,53 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.my_toolbar))
 
 
-        val keyboard = findViewById<LinearLayout>(R.id.keyboard)
+        createKayboard()
 
+    }
+
+    fun createKayboard(){
+
+        val keyboard = findViewById<LinearLayout>(R.id.keyboard)
+        keyboard.setOrientation(LinearLayout.VERTICAL)
+        keyboard.setGravity(Gravity.CENTER)
         //crear un teclado en código
+
+
+        for (i in 0 .. 3){
+            val fila = LinearLayout(this)
+            fila.setOrientation(LinearLayout.HORIZONTAL)
+            fila.setGravity(Gravity.CENTER)
+
+            for (j in 0 .. 3){
+
+                val button = Button(this)
+                button.setText("hola"+i)
+                button.setHeight(250)
+                button.setWidth(250)
+
+
+                fila.addView(button)
+
+            }
+            keyboard.addView(fila)
+
+
+
+        }
+        val fila = LinearLayout(this)
+        fila.setOrientation(LinearLayout.HORIZONTAL)
+        fila.setGravity(Gravity.CENTER)
+
+        val buttonResult = Button(this)
+        buttonResult.setText("RESULTADO")
+        buttonResult.setHeight(250)
+        buttonResult.setWidth(1500)
+
+        fila.addView(buttonResult)
+        keyboard.addView(fila)
+
+
+
 
     }
 }
